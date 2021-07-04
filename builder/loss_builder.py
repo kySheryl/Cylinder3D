d1b8fe34451994007cd71c0c46d18b58320f1ff4
Yue Kuang <kuangyue@umich.edu>
2021-07-04 15:19:13 +0800

    add weight to loss function

--- builder/loss_builder.py
+++ builder/loss_builder.py
@@ -7,8 +7,16 @@ from utils.lovasz_losses import lovasz_softmax
 
 
 def build(wce=True, lovasz=True, num_class=20, ignore_label=0):
-
-    loss_funs = torch.nn.CrossEntropyLoss(ignore_index=ignore_label)
+    
+    ####add weights to loss function
+    weights = torch.tensor([0.000000003909961577, 0.000000002624572561, 0.0000005611360536, 0.0000002701413325, 0.00000009464693947,
+                        0.0000000558845635, 0.0000003391193883, 0.000000654810236, 0.000002521495751, 0.0000000006033565441,
+                        0.000000008024802933, 0.0000000008488103755, 0.00000003767766337, 0.0000000009669924, 0.000000001877186965,
+                        0.0000000004337601619, 0.00000001693416953, 0.000000001508196626, 0.00000004238356704, 0.0000002032001999]).cuda()
+    loss_funs = torch.nn.CrossEntropyLoss(ignore_index=ignore_label,weight=weights)
+    ############
+    
+    # loss_funs = torch.nn.CrossEntropyLoss(ignore_index=ignore_label)
 
     if wce and lovasz:
         return loss_funs, lovasz_softmax
