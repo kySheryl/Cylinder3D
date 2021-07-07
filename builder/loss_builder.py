5897a5065f0ee54ae8f5c750ca2b17c004889219
kySheryl <59666549+kySheryl@users.noreply.github.com>
2021-07-07 11:00:15 +0800

    Update loss_builder.py

--- builder/loss_builder.py
+++ builder/loss_builder.py
@@ -8,7 +8,7 @@ from utils.lovasz_losses import lovasz_softmax
 
 def build(wce=True, lovasz=True, num_class=20, ignore_label=0):
     
-    ####add weights to loss function
+    ####weighted loss function
     weights = torch.tensor([0.000000003909961577, 0.000000002624572561, 0.0000005611360536, 0.0000002701413325, 0.00000009464693947,
                         0.0000000558845635, 0.0000003391193883, 0.000000654810236, 0.000002521495751, 0.0000000006033565441,
                         0.000000008024802933, 0.0000000008488103755, 0.00000003767766337, 0.0000000009669924, 0.000000001877186965,
@@ -16,7 +16,9 @@ def build(wce=True, lovasz=True, num_class=20, ignore_label=0):
     loss_funs = torch.nn.CrossEntropyLoss(ignore_index=ignore_label,weight=weights)
     ############
     
+    #### unweighted loss function
     # loss_funs = torch.nn.CrossEntropyLoss(ignore_index=ignore_label)
+    #############3
 
     if wce and lovasz:
         return loss_funs, lovasz_softmax
